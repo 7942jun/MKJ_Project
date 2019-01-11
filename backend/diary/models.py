d60c771369193756d8e244fd6ad9633800f20e21
7942jun <7942jun@github.com>
2019-01-12 00:53:29 +0900

    add memory models

--- backend/diary/models.py
+++ backend/diary/models.py
@@ -6,4 +6,9 @@ class User(models.Model):
     name = models.CharField(max_length=8)
     email = models.CharField(max_length=30)
 
+class Memory(models.Model):
+	title = models.CharField(max_length=50)
+	content = models.TextField()
+	create_date = models.DateTimeField(auto_now_add=True)
+	modify_date = models.DateTimeField(auto_now=True)
 
